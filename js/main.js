f5dbb1f8fb1e3660ac2c07429c4535a3d2d65b5b
luizluizluiz <luizsantos.projects@gmail.com>
2015-12-16 02:59:22 +0800

    Added Game of Life Logic and Initial Darth Vader Template for the grid as tribute to Star Wars :)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Script Notes:
 // 14-12-2015: Coded canvas properties
-
+// 15-12-2015: Applied GOL Logic and basic buttons
 
 // GOL Properties and Behavior
 var life = (function () {
@@ -8,22 +8,150 @@ var life = (function () {
         xCells,
         prev = [], // previous generation
         next = [], // next generation
-        speed = 100,
+        speed = 100, // default speed
         timeout,
         alive = false,
         x,
-        y;
+        y,
+        underPopulated = 2,
+        stablePopulation = 3;
+
+
+    // adjust cell processing speed
+    function adjustCellSpeed(faster) {
+        if (faster) {
+            // if speed is set to 0 don't do anything
+            if (life.speed === 0) {
+                return;
+            }
+            life.speed -= 10;
+
+        } else {
+            // if speed is greated than 1k dont do anything
+            if (life.speed === 990) {
+                return;
+            }
+            life.speed += 10;
+        }
+        if (alive) {
+            clearInterval(timeout);
+            timeout = setInterval(life.nextLife, life.speed);
+        }
+    }
+
+    // Toggle cell grid's behaviour based on speed
+    function startStopLife() {
+        if (!alive) {
+            alive = true;
+            timeout = setInterval(life.nextLife, this.speed);
+        } else {
+            alive = false;
+            clearInterval(timeout);
+        }
+    }
+
+    // Check the grid's neighbor count    
+    function checkNeighbours(x, y) {
+        var l = life,
+            count = 0,
+            i,
+            neighbours = [
+                life.prev[x][(y - 1 + life.yCells) % life.yCells], 
+                life.prev[(x + 1 + life.xCells) % life.xCells][(y - 1 + life.yCells) % life.yCells],
+                life.prev[(x + 1 + life.xCells) % life.xCells][y],
+                life.prev[(x + 1 + life.xCells) % life.xCells][(y + 1 + life.yCells) % life.yCells],
+                life.prev[x][(y + 1 + life.yCells) % life.yCells],
+                life.prev[(x - 1 + life.xCells) % life.xCells][(y + 1 + life.yCells) % life.yCells],
+                life.prev[(x - 1 + life.xCells) % life.xCells][y],
+                life.prev[(x - 1 + life.xCells) % life.xCells][(y - 1 + life.yCells) % life.yCells]
+            ];
+
+        for (i = 0; i < neighbours.length; i++) {
+            // if neighbor cell state is true increment neighbor alive count
+            if (neighbours[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+    // Logic for Cell Behavior
+    function nextLife() {
+        var l = life,
+            g = graphics,
+            count;
+
+        // Pass prev generation cell stat to next generation
+        for (x = 0; x < life.xCells; x++) {
+            for (y = 0; y < life.yCells; y++) {
+                life.next[x][y] = life.prev[x][y];
+            }
+        }
+
+        for (x = 0; x < life.xCells; x++) {
+            for (y = 0; y < life.yCells; y++) {
+                count = checkNeighbours(x, y);
+            
+               if (prev[x][y]) {
+
+                    // If cell is under populated or overpopulated : Cell will die
+                    if (count < underPopulated || count > stablePopulation) {
+                        next[x][y] = false;
+                        graphics.drawCell(x, y, life.next[x][y]);
+                    }
+
+                // Cell will live
+                } else if (count === stablePopulation) {
+                    next[x][y] = true;
+                    graphics.drawCell(x, y, life.next[x][y]);
+                }
+            }
+        }
+
+        // Next generation will become the current generation
+        for (x = 0; x < life.xCells; x++) {
+            for (y = 0; y < life.yCells; y++) {
+                life.prev[x][y] = life.next[x][y];
+            }
+        }
+
+    }
+
+
+    // Clear Grid Function
+    function reset() {
+        var x,
+            y;
+
+        // Select all next generation cell state and clear
+        for (x = 0; x < life.xCells; x++) {
+            for (y = 0; y < life.yCells; y++) {
+                life.next[x][y] = false;
+                graphics.drawCell(x, y, life.next[x][y]);
+            }
+        }
+
+        // Select all previous generation cell state and clear
+        for (x = 0; x < life.xCells; x++) {
+            for (y = 0; y < life.yCells; y++) {
+                life.prev[x][y] = false;
+                graphics.drawCell(x, y, life.prev[x][y]);
+            }
+        }
+    }
 
     function initLifeGrid(canvasSelector) {
         graphics.initCanvas(canvasSelector);
         life.xCells = Math.floor((graphics.canvas.width - 1) / graphics.cellSize);
         life.yCells = Math.floor((graphics.canvas.height - 1) / graphics.cellSize);
-        
 
         graphics.ctx.fillStyle = graphics.offColour;
         graphics.ctx.fillRect(0, 0, life.xCells * graphics.cellSize, life.yCells * graphics.cellSize);
         graphics.ctx.fillStyle = graphics.gridColour;
 
+        // Create Grid Logic
         for (x = 0; x < life.xCells; x++) {
             life.prev[x] = [];
             life.next[x] = [];
@@ -55,7 +183,11 @@ var life = (function () {
         universe: prev,
         speed: speed,
         initLifeGrid: initLifeGrid,
-        isAlive: isAlive
+        isAlive: isAlive,
+        reset: reset,
+        nextLife: nextLife,
+        startStopLife: startStopLife,
+        adjustCellSpeed: adjustCellSpeed
     };
 }());
 
@@ -76,6 +208,7 @@ var graphics = (function () {
         graphics.canvasSelector = canvasSelector;
     }
 
+    // Fill specific grid based on alive variable
     function drawCell(x, y, alive) {
         graphics.ctx.fillStyle = (alive) ? onColour : offColour;
         graphics.ctx.fillRect(x * cellSize + 1, y * cellSize + 1, cellSize - 1, cellSize - 1);
@@ -99,6 +232,7 @@ var graphics = (function () {
                 state = !life.prev[x][y];
             }
             life.prev[x][y] = state;
+            console.log("life.drawCell(" + x + "," + y + ", true");
             drawCell(x, y, state);
         }
 
